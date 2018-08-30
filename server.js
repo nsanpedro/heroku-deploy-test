0fb52f306b11748b422ffd9a815a551b51a655d2
nicosp <nicolassanpedro@gmail.com>
2018-08-29 22:13:29 -0300

    1st commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 8080;
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+      if(err) {
+        console.log('There was an error on server.log file', err);
+      }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+
+app.get('/', (req, res) => {
+    // res.send('Hello world once again');
+    res.render('home.hbs', {
+        pageTitle: 'THIS IS THE HOME BRUH',
+        textInfo: 'This is all kinds of nonsense bullshit'
+    });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+      pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
